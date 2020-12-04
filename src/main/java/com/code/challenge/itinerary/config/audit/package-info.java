0608524c2e3b2357b900455b5d1d57a9ed5973e2
Bob Shi <bob.y.shi@pwd.com>
2020-12-04 16:04:03 +0800

    Initial version of itinerary generated by JHipster-6.5.1

---
+++ src/main/java/com/code/challenge/itinerary/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.code.challenge.itinerary.config.audit;
---
+++ src/main/java/com/code/challenge/itinerary/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.code.challenge.itinerary.repository;
+
+import com.code.challenge.itinerary.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
